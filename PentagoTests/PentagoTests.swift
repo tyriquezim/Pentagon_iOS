acb8e7eaa594a133d4d871a33b01d0e960a685d8
tyriquezim <zimbizi.tyrique@gmail.com>
2025-12-06 14:20:55 +0800

    Initial Commit

---
+++ PentagoTests/PentagoTests.swift
@@ -0,0 +1,17 @@
+//
+//  PentagoTests.swift
+//  PentagoTests
+//
+//  Created by Tyrique Zimbizi on 6/12/2025.
+//
+
+import Testing
+@testable import Pentago
+
+struct PentagoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
