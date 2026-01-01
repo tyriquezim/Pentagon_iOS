8d966f5eccfd6e6ac657287e2bb73fb3d1593f8e
tyriquezim <zimbizi.tyrique@gmail.com>
2026-01-02 06:23:53 +0800

    Successfully completed the HowToPlayScreen with the game description and rules

---
+++ Pentago/DataStores/GameRulesInfoStore.swift
@@ -0,0 +1,13 @@
+//
+//  GameRulesInfo.swift
+//  Pentago
+//
+//  Created by Tyrique Zimbizi on 1/1/2026.
+//
+
+struct GameRulesInfo
+{
+    var gameDescription: String
+    var gameRulesArray: Array<(ruleTitle: String, ruleDescription: String)>
+    
+}
